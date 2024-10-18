d770ae6234f5fb205a645aaf99986e1ceb967751
joel kirabo <kirabojoel001@gmail.com>
2024-10-18 19:06:08 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-r from-orange-200 to-orange-300 min-h-screen">     
+         home
+    </div>
+
+  );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/Conatainer";
+export default function Products() {
+  return (
+    <div className="bg-gradient-to-r from-orange-200 to-orange-300 min-h-screen">     
+         <Container/>
+    </div>
+
+  );
+}
